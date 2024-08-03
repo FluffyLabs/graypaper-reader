354479dc1b5410f9a459dbdcece819fc3aa5f622
Tomasz Drwięga <tomusdrw@gmail.com>
2024-08-03 23:24:56 +0200

    Nested outline.

--- src/components/Outline/Outline.tsx
+++ src/components/Outline/Outline.tsx
@@ -1,5 +1,6 @@
 import './Outline.css';
-import type { InDocLocation, Outline } from '../../utils/IframeController';
+import type { InDocLocation, Outline, OutlineItem } from '../../utils/IframeController';
+import {ReactNode, useCallback, useMemo} from 'react';
 
 type OutlineProps = {
   outline: Outline,
@@ -10,26 +11,57 @@ type OutlineProps = {
 export function Outline({
   outline,
   location,
-  jumpTo
+  jumpTo,
 } :  OutlineProps) {
-  return <>
+  const nestedOutline = useMemo(() => {
+    const nested = {} as { [key: string]: Outline };
+    for (const o of outline) {
+      const number = o.text.split('.')[0].replace('Appendix ', '');
+      const arr = nested[number] ?? [];
+      arr.push(o);
+      nested[number] = arr;
+    }
+    return nested;
+  }, [outline]);
+
+  return (
     <div className="outline">
       <ul>
-        {outline.map(x => (
-          <li key={x.id} className={isSameSection(x.text, location) ? 'active' : ''}>
-            <a onClick={() => jumpTo(x.id)}>
-              {x.text}
-            </a>
-          </li>
+        {Object.entries(nestedOutline).map(([_, items]) => (
+          <Item key={items[0].id} item={items[0]} location={location} jumpTo={jumpTo}>
+            {items.length > 1 && (
+              <ul>
+                {items.slice(1).map(i => <Item key={i.id} item={i} location={location} jumpTo={jumpTo} />)}
+              </ul>
+            )}
+          </Item>
         ))}
       </ul>
     </div>
-  </>
+  );
+}
+type ItemProps = {
+  item: OutlineItem,
+  location: InDocLocation,
+  jumpTo: OutlineProps["jumpTo"],
+  children?: ReactNode,
+};
+function Item({item, location, jumpTo, children }: ItemProps) {
+  const handleClick = useCallback(() => {
+    jumpTo(item.id);
+  }, [jumpTo, item]);
+
+  return (
+    <li className={isSameSection(item.text, location) ? 'active' : ''}>
+      <a onClick={handleClick}>{item.text}</a>
+      {children}
+    </li>
+  );
 }
 
 function isSameSection(name: string, location: InDocLocation) {
-  const matchesSection = name.indexOf(location.section?.title ?? '') !== -1;
-  const matchesSubSection = name.indexOf(location.subSection?.title ?? '') !== -1;
+  const matchesSection = location.section && name.indexOf(location.section.title) !== -1;
+  const matchesSubSection = location.subSection && name.indexOf(location.subSection.title ?? '') !== -1;
 
   return matchesSection || matchesSubSection;
 }
