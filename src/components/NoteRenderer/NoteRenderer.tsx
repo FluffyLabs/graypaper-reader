34e3bf6855d61016d0deb75fbfca52de165138c3
Sebastian <sebastian.koszuta@gmail.com>
2024-10-01 14:02:53 +0200

    Rendering issues on resize and scroll (#70)
    * Note placement and re-rendering improvements
    - Note placement avoids covering up text in the other column
    - Notes are re-rendered on viewport resize
    
    * Fixed rendering bugs when resizing app
    
    * Fixed crashing on scrolling with an active selection
    
    * Increased contrast in light mode

--- src/components/NoteRenderer/NoteRenderer.tsx
+++ src/components/NoteRenderer/NoteRenderer.tsx
@@ -1,68 +1,13 @@
-import { useContext, useEffect, useMemo, useRef, useState } from "react";
-import { useThrottle } from "../../hooks/useThrottle";
-import { subtractBorder } from "../../utils/subtractBorder";
+import { useContext, useMemo } from "react";
 import { type INotesContext, NotesContext } from "../NotesProvider/NotesProvider";
 import { type IPdfContext, PdfContext } from "../PdfProvider/PdfProvider";
 import { HighlightNote } from "./components/HighlightNote/HighlightNote";
 import { PointNote } from "./components/PointNote/PointNote";
 
-const SCROLL_THROTTLE_DELAY_MS = 100;
-
-function isPartlyInViewport({ top, bottom }: DOMRect) {
-  const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-
-  return (
-    (top <= 0 && bottom > 0) ||
-    (top < viewportHeight && bottom >= viewportHeight) ||
-    (top > 0 && bottom < viewportHeight)
-  );
-}
-
 export function NoteRenderer() {
-  const [visiblePages, setVisiblePages] = useState<number[]>([]);
-  const viewerRootEventHandlerRef = useRef<() => void>();
   const { notes } = useContext(NotesContext) as INotesContext;
   const { viewer } = useContext(PdfContext) as IPdfContext;
-
-  const handleViewChanged = useThrottle(() => {
-    if (!viewer) return;
-
-    const visiblePagesAfterEvent: number[] = [];
-
-    for (let i = 0; i < viewer.pagesCount; i++) {
-      if (isPartlyInViewport(viewer.getPageView(i).div.getBoundingClientRect())) {
-        visiblePagesAfterEvent.push(i + 1); // using page numbers instead of indices
-      }
-    }
-
-    setVisiblePages((visiblePages) =>
-      visiblePages.join(";") !== visiblePagesAfterEvent.join(";") ? visiblePagesAfterEvent : visiblePages,
-    );
-  }, SCROLL_THROTTLE_DELAY_MS);
-
-  useEffect(() => {
-    if (!viewer) return;
-
-    if (viewer.container) {
-      if (viewerRootEventHandlerRef.current) {
-        viewer.container.removeEventListener("scroll", viewerRootEventHandlerRef.current);
-      }
-
-      viewer.container.addEventListener("scroll", handleViewChanged);
-      viewerRootEventHandlerRef.current = handleViewChanged;
-    }
-
-    return () => {
-      if (viewerRootEventHandlerRef.current) {
-        viewer.container.removeEventListener("scroll", viewerRootEventHandlerRef.current);
-      }
-    };
-  }, [viewer, handleViewChanged]);
-
-  useEffect(() => {
-    if (!viewer) return;
-    viewer.container.dispatchEvent(new CustomEvent("scroll"));
-  }, [viewer]);
+  const { visiblePages, pageOffsets } = useContext(PdfContext) as IPdfContext;
 
   const notesToRender = useMemo(
     () => notes.filter((note) => visiblePages.includes(note.pageNumber)),
@@ -72,21 +17,12 @@ export function NoteRenderer() {
   return notesToRender.map((note) => {
     if (!viewer) return;
 
-    const pageElement = viewer.getPageView(note.pageNumber - 1)?.div;
-
-    if (!pageElement) return null;
-
-    const pageOffset = subtractBorder(
-      new DOMRect(pageElement.offsetLeft, pageElement.offsetTop, pageElement.offsetWidth, pageElement.offsetHeight),
-      pageElement,
-    );
-
     if ("left" in note && "top" in note) {
-      return <PointNote note={note} pageOffset={pageOffset} key={note.date} />;
+      return <PointNote note={note} pageOffset={pageOffsets[note.pageNumber]} key={note.date} />;
     }
 
     if ("selectionStart" in note && "selectionEnd" in note && "selectionString" in note) {
-      return <HighlightNote note={note} pageOffset={pageOffset} key={note.date} />;
+      return <HighlightNote note={note} pageOffset={pageOffsets[note.pageNumber]} key={note.date} />;
     }
 
     throw new Error("Unidentified note type.");
--- src/components/SelectionRenderer/SelectionRenderer.tsx
+++ src/components/SelectionRenderer/SelectionRenderer.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { usePrevious } from "../../hooks/usePrevious";
 import { subtractBorder } from "../../utils/subtractBorder";
 import type { ISynctexBlock } from "../CodeSyncProvider/CodeSyncProvider";
@@ -14,19 +14,11 @@ export function SelectionRenderer() {
   const { selectedBlocks, pageNumber, scrollToSelection, setScrollToSelection, setSelectionString } = useContext(
     SelectionContext,
   ) as ISelectionContext;
+  const { pageOffsets } = useContext(PdfContext) as IPdfContext;
   const [textLayerRendered, setTextLayerRendered] = useState<number[]>([]);
   const previousScrollToSelection = usePrevious(scrollToSelection);
 
-  const pageOffset = useMemo(() => {
-    if (!viewer || pageNumber === null) return null;
-
-    const pageElement = viewer.getPageView(pageNumber - 1)?.div;
-
-    return subtractBorder(
-      new DOMRect(pageElement.offsetLeft, pageElement.offsetTop, pageElement.offsetWidth, pageElement.offsetHeight),
-      pageElement,
-    );
-  }, [pageNumber, viewer]);
+  const pageOffset = pageNumber ? pageOffsets[pageNumber] : null;
 
   useEffect(() => {
     if (!viewer || (!previousScrollToSelection && scrollToSelection)) return;
@@ -69,7 +61,7 @@ export function SelectionRenderer() {
     if (!viewer || !selectedBlocks.length || pageNumber === null || !textLayerRendered.includes(pageNumber)) return;
 
     const pageElement = viewer.getPageView(pageNumber - 1)?.div;
-    const textLayerElement = viewer.getPageView(pageNumber - 1)?.textLayer.div;
+    const textLayerElement = viewer.getPageView(pageNumber - 1)?.textLayer?.div;
     const selection = document.getSelection();
 
     if (!pageElement || !textLayerElement || !selection) return;
