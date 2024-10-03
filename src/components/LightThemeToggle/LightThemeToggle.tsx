cf310d173050517679037846bdf463f67ec7813d
Tomasz Drwięga <tomusdrw@gmail.com>
2024-10-03 11:00:42 +0200

    Dark -> Gray -> Light theme.

--- src/components/LightThemeToggle/LightThemeToggle.tsx
+++ src/components/LightThemeToggle/LightThemeToggle.tsx
@@ -1,22 +1,23 @@
 import { useCallback, useContext } from "react";
 import { Tooltip } from "react-tooltip";
-import { type IPdfContext, PdfContext } from "../PdfProvider/PdfProvider";
+import { type IPdfContext, PdfContext, themesOrder } from "../PdfProvider/PdfProvider";
 
 export function LightThemeToggle() {
-  const { lightThemeEnabled, setLightThemeEnabled } = useContext(PdfContext) as IPdfContext;
+  const { theme, setTheme } = useContext(PdfContext) as IPdfContext;
 
   const handleButtonClick = useCallback(() => {
-    setLightThemeEnabled(!lightThemeEnabled);
-  }, [setLightThemeEnabled, lightThemeEnabled]);
+    const nextTheme = themesOrder[themesOrder.indexOf(theme) + 1];
+    setTheme(nextTheme ?? themesOrder[0]);
+  }, [setTheme, theme]);
 
   return (
     <div>
       <button
         data-tooltip-id="theme"
-        data-tooltip-content="Toggle between light and dark theme"
+        data-tooltip-content="Toggle between light, gray and dark themes"
         data-tooltip-place="right"
         onClick={handleButtonClick}
-        className={`light-theme-toggle ${lightThemeEnabled ? "toggled" : ""}`}
+        className={`light-theme-toggle ${theme === "light" ? "toggled" : ""}`}
       >
         💡
       </button>
