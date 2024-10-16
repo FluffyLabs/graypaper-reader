368d0cadf260f84a84ff9bab9424982c704ca0ce
Tomek Drwięga <tomusdrw@users.noreply.github.com>
2024-10-16 14:46:50 +0200

    Initial version of matrix bot to listen for links to the reader. (#92)

---
+++ tools/matrix-bot/logger.ts
@@ -0,0 +1,45 @@
+import { type WriteStream, createWriteStream } from "node:fs";
+
+export class MessagesLogger {
+  private readonly file: WriteStream;
+
+  constructor(
+    private readonly roomId: string,
+    fileName: string,
+  ) {
+    this.file = createWriteStream(fileName, {
+      encoding: "utf8",
+      flags: "a+",
+      flush: true,
+    });
+  }
+
+  private generatePermalink(eventId: string): string {
+    return `https://matrix.to/#/${this.roomId}/${eventId}`;
+  }
+
+  onMessage(msg: string, sender: string | undefined, eventId: string | undefined, date: Date | null) {
+    if (!eventId) {
+      return;
+    }
+    if (!msg.includes("graypaper.fluffylabs.dev")) {
+      return;
+    }
+
+    const link = this.generatePermalink(eventId);
+
+    this.file.write(
+      JSON.stringify({
+        date,
+        sender,
+        link,
+        msg,
+      }),
+    );
+    this.file.write(",\n");
+  }
+
+  flush() {
+    this.file.end();
+  }
+}
