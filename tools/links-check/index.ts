35e240a5be5c2fee0601b31e2a91f52df687ca16
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2024-12-16 16:35:33 +0100

    Added ignore file option to link checker Handling of process arguments with commander

--- tools/links-check/index.ts
+++ tools/links-check/index.ts
@@ -1,7 +1,11 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
 import fastGlob from "fast-glob";
+import ignore from "ignore";
 import { fetchMetadata } from "./metadata";
 import { type Report, printReport } from "./report";
 import { getCommonPath, scan } from "./scan";
+import { program } from "commander";
 
 main().catch((err: unknown) => {
   console.error(`🚨 ${err}`);
@@ -9,37 +13,60 @@ main().catch((err: unknown) => {
 });
 
 async function main() {
-  const args = process.argv.slice(2);
+  program
+    .argument("<paths...>", "Paths of files to be scanned. Supports glob patterns.")
+    .option("--ignore-file <path>", "Path to the ignore file.")
+    .action(async (paths, options) => {
+      let files = await fastGlob(paths);
+      const commonPath = getCommonPath(files);
 
-  if (args.length === 0) {
-    throw new Error("Provide a list of files to scan.");
-  }
+      if (options.ignoreFile) {
+        let ignorePatterns: string[];
 
-  const files = await fastGlob(args);
-  const commonPath = getCommonPath(files);
+        try {
+          const ignoreFileContent = await fs.promises.readFile(path.resolve(options.ignoreFile), "utf-8");
+          ignorePatterns = ignoreFileContent.split("\n").filter(Boolean);
+          console.info(`Using ignore file: ${options.ignoreFile}`);
+        } catch (err) {
+          console.error(`Failed to read ignore file: ${err}`);
+          process.exit(1);
+        }
 
-  console.info();
+        if (ignorePatterns.length) {
+          const ignoreManager = ignore().add(ignorePatterns);
+          files = files.filter((file) => !ignoreManager.ignores(path.relative(commonPath, file)));
+        }
+      }
 
-  const metadata = await fetchMetadata();
+      console.info();
 
-  const label = `scanning ${files.length}`;
-  console.time(label);
-  let report: Report | null = null;
-  try {
-    report = await scan(files, metadata, commonPath);
-  } finally {
-    console.timeEnd(label);
-  }
-  console.info();
-  console.info();
+      const metadata = await fetchMetadata();
 
-  if (!report) {
-    return;
-  }
+      const label = `scanning ${files.length}`;
+      console.time(label);
+      let report: Report | null = null;
+      try {
+        report = await scan(files, metadata, commonPath);
+      } finally {
+        console.timeEnd(label);
+      }
+      console.info();
+      console.info();
 
-  const summary = printReport(report);
+      if (!report) {
+        return;
+      }
 
-  if (summary.broken > 0) {
-    process.exit(1);
+      const summary = printReport(report);
+
+      if (summary.broken > 0) {
+        process.exit(1);
+      }
+    });
+
+  await program.parseAsync(process.argv);
+
+  if (!program.args.length) {
+    program.help();
   }
 }
