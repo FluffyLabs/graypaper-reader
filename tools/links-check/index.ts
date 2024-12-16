2ac9ee32b1028e8e165b25822552b6831cfa5edf
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2024-12-16 14:02:07 +0100

    Reverted changes which will be moved to new PRs

--- tools/links-check/index.ts
+++ tools/links-check/index.ts
@@ -1,9 +1,5 @@
-import * as fs from "node:fs";
-import * as path from "node:path";
-import fg from "fast-glob";
-import ignore from "ignore";
+import fastGlob from "fast-glob";
 import { fetchMetadata } from "./metadata";
-import { performMigrations, promptUserForMigration } from "./migrate";
 import { type Report, printReport } from "./report";
 import { getCommonPath, scan } from "./scan";
 
@@ -12,79 +8,25 @@ main().catch((err: unknown) => {
   process.exit(255);
 });
 
-async function findGitignore(startPath: string): Promise<string | null> {
-  let currentPath = startPath;
-  while (true) {
-    const gitignorePath = path.join(currentPath, ".gitignore");
-    try {
-      await fs.promises.access(gitignorePath);
-      return gitignorePath;
-    } catch (err) {
-      const parentPath = path.dirname(currentPath);
-      if (parentPath === currentPath) {
-        break;
-      }
-      currentPath = parentPath;
-    }
-  }
-  return null;
-}
-
 async function main() {
   const args = process.argv.slice(2);
 
-  const ignoreFileIndex = args.indexOf("--ignore-file");
-  let ignorePatterns: string[] = [];
-
-  const files =
-    ignoreFileIndex === -1
-      ? args
-      : args.filter((arg, index) => index !== ignoreFileIndex && index !== ignoreFileIndex + 1);
-
-  if (files.length === 0) {
+  if (args.length === 0) {
     throw new Error("Provide a list of files to scan.");
   }
 
-  const expandedFiles = await fg(files);
-  const commonPath = getCommonPath(expandedFiles);
-
-  if (ignoreFileIndex !== -1 && args[ignoreFileIndex + 1]) {
-    const ignoreFilePath = args[ignoreFileIndex + 1];
-    try {
-      const ignoreFileContent = await fs.promises.readFile(path.resolve(ignoreFilePath), "utf-8");
-      ignorePatterns = ignoreFileContent.split("\n").filter(Boolean);
-      console.info(`Using ignore file: ${ignoreFilePath}`);
-    } catch (err) {
-      console.error(`Failed to read ignore file: ${err}`);
-      process.exit(1);
-    }
-  } else {
-    try {
-      const gitignorePath = await findGitignore(commonPath);
-      if (gitignorePath) {
-        const gitignoreContent = await fs.promises.readFile(gitignorePath, "utf-8");
-        ignorePatterns = gitignoreContent.split("\n").filter(Boolean);
-        console.info(`Using .gitignore file: ${gitignorePath}`);
-      } else {
-        console.warn("No .gitignore file found.");
-      }
-    } catch (err) {
-      console.warn(`Failed to read .gitignore file: ${err}`);
-    }
-  }
+  const files = await fastGlob(args);
+  const commonPath = getCommonPath(files);
 
   console.info();
 
-  const ig = ignore().add(ignorePatterns);
-  const filteredFiles = expandedFiles.filter((file) => !ig.ignores(path.relative(commonPath, file)));
-
   const metadata = await fetchMetadata();
 
-  const label = `scanning ${filteredFiles.length}`;
+  const label = `scanning ${files.length}`;
   console.time(label);
   let report: Report | null = null;
   try {
-    report = await scan(filteredFiles, metadata, commonPath);
+    report = await scan(files, metadata, commonPath);
   } finally {
     console.timeEnd(label);
   }
@@ -95,10 +37,9 @@ async function main() {
     return;
   }
 
-  printReport(report);
+  const summary = printReport(report);
 
-  const applyMigrations = await promptUserForMigration();
-  if (applyMigrations) {
-    await performMigrations(report, commonPath);
+  if (summary.broken > 0) {
+    process.exit(1);
   }
 }
