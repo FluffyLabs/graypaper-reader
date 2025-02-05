cc47d3006b4572d594b392a66b52584c981d654e
DrEverr <ms1997sokol@gmail.com>
2025-02-05 13:23:49 +0100

    use " instead of '

--- scripts/cp-gp.js
+++ scripts/cp-gp.js
@@ -1,7 +1,7 @@
-import shell from 'shelljs';
+import shell from "shelljs";
 
 // Tworzenie katalogu public, jeśli nie istnieje
-shell.mkdir('-p', 'public');
+shell.mkdir("-p", "public");
 
 // Kopiowanie plików z graypaper-archive/dist do public
-shell.cp('-r', 'graypaper-archive/dist/*', 'public');
+shell.cp("-r", "graypaper-archive/dist/*", "public");
