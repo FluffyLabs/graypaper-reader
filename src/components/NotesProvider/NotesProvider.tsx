efad22df978b1ab4732995e3b9e7a7507f5bcba0
DrEverr <ms1997sokol@gmail.com>
2025-02-26 10:00:56 +0100

    refactor

--- src/components/NotesProvider/NotesProvider.tsx
+++ src/components/NotesProvider/NotesProvider.tsx
@@ -94,14 +94,7 @@ export function NotesProvider({ children }: INotesProviderProps) {
 
   const allNotesReady = useMemo(() => localNotesReady && remoteNotesReady, [localNotesReady, remoteNotesReady]);
 
-  const [filteredNotes, labels, toggleLabel] = useLabels(allNotes);
-
-  const handleToggleLabel = useCallback(
-    (label: ILabel) => {
-      toggleLabel(label);
-    },
-    [toggleLabel],
-  );
+  const [filteredNotes, labels, handleToggleLabel] = useLabels(allNotes);
 
   const context: INotesContext = {
     notesPinned,
