f6c0c1826a71c7e131c4d258aa2c146660a79260
Wojciech Piątkowski <chmurson@gmail.com>
2025-06-16 22:46:54 +0200

    force dark mode

--- src/components/LightDarkThemeSyncer/LightDarkThemeSyncer.tsx
+++ src/components/LightDarkThemeSyncer/LightDarkThemeSyncer.tsx
@@ -1,17 +1,9 @@
-import { useIsDarkMode } from "@krystian5011/shared-ui";
 import { useEffect } from "react";
 
 export const LightDarkThemeSyncer = () => {
-  const isDarkMode = useIsDarkMode();
-
   useEffect(() => {
-    const htmlElement = document.querySelector("html");
-
-    if (htmlElement) {
-      htmlElement.classList.toggle("dark", isDarkMode);
-      htmlElement.classList.toggle("light", !isDarkMode);
-    }
-  }, [isDarkMode]);
+    document.documentElement.classList.toggle("dark", true);
+  }, []);
 
   return null;
 };
