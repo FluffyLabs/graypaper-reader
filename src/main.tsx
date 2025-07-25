57f73cf3a90639751923e8aa2f59f1380ed23acc
Tomasz Drwięga <tomusdrw@gmail.com>
2025-07-25 09:42:54 +0200

    Redirect to beta.

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import "./font.css";
 import { LocationProvider } from "./components/LocationProvider/LocationProvider.tsx";
 import { MetadataProvider } from "./components/MetadataProvider/MetadataProvider.tsx";
 
+redirectToBeta("graypaper-reader.netlify.app");
+
 Modal.setAppElement("#root");
 
 ReactDOM.createRoot(document.getElementById("root") ?? document.body).render(
@@ -18,3 +20,21 @@ ReactDOM.createRoot(document.getElementById("root") ?? document.body).render(
     </MetadataProvider>
   </React.StrictMode>,
 );
+
+/**
+ * By opting in by setting `localStorage.BETA_REDIRECT` the user
+ * can automatically be redirect to beta version of the tool.
+ */
+export function redirectToBeta(betaHost: string) {
+  try {
+    const shouldRedirectLocal = localStorage.getItem("BETA_REDIRECT");
+    if (shouldRedirectLocal === undefined) {
+      return;
+    }
+    const url = new URL(window.location.href);
+    url.host = betaHost;
+    window.location.href = url.toString();
+  } catch (e) {
+    console.warn(`Error redirecting to beta: ${e}`);
+  }
+}
