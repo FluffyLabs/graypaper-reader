d27fcb8f19c88085b162bf94a5e9d290f9dc1d00
Wojciech Piątkowski <chmurson@gmail.com>
2025-08-20 16:56:56 +0200

    feat: added skeleton for outline

--- src/components/Outline/Outline.tsx
+++ src/components/Outline/Outline.tsx
@@ -1,6 +1,6 @@
 import "./Outline.css";
 import type { PDFDocumentProxy } from "pdfjs-dist";
-import { FC, memo, type ReactNode, useCallback, useContext, useEffect, useState } from "react";
+import { type FC, type ReactNode, memo, useCallback, useContext, useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { type ILocationContext, LocationContext } from "../LocationProvider/LocationProvider";
 import { PdfContext } from "../PdfProvider/PdfProvider";
@@ -36,58 +36,77 @@ export function Outline({ searchIsDone }: { searchIsDone: boolean }) {
       return undefined;
     };
 
-    const itemToScrollTo =  !outline ? undefined : findItem(outline);
+    const itemToScrollTo = !outline ? undefined : findItem(outline);
     if (itemToScrollTo?.dest) {
       linkService?.goToDestination(itemToScrollTo.dest);
     }
   }, [searchIsDone, section, outline, linkService]);
 
-
-  const handleClick = useCallback((dest: TOutline[0]['dest']) => {
-    if (!dest) return;
-    linkService?.goToDestination(dest);
-  }, [linkService]);
-
-  return (
-    <OutlineDumb outline={outline} onClick={handleClick} />
+  const handleClick = useCallback(
+    (dest: TOutline[0]["dest"]) => {
+      if (!dest) return;
+      linkService?.goToDestination(dest);
+    },
+    [linkService],
   );
+
+  return <OutlineDumb outline={outline} onClick={handleClick} />;
 }
 
-const OutlineDumb: FC<{outline?: TOutline, onClick: (item: TOutline[0]['dest']) => void}> = memo(({outline, onClick}) => {
-  if (!outline) return <div>Loading...</div>;
+type IOutline = Pick<TOutline[0], "title" | "dest" | "items">;
 
-  const renderOutline = (outline: TOutline, options: { firstLevel?: boolean } = {}) => {
-    const { firstLevel = false } = options;
+const outlineForSkeleton = [
+  ...Array(12).fill(0).map((_,index) => ({
+    title: `Skeleton Title ${index + 1}`,
+    dest: "skeleton-destination",
+    items: [
+      ...Array(5).fill(0).map((_, index) => ({
+        title: `Skeleton Subitem ${index + 1}`,
+        dest: "skeleton-subitem-destination",
+        items: []
+      })),
+    ],
+  }))
+] satisfies IOutline[];
+
+const OutlineDumb: FC<{ outline?: IOutline[]; onClick: (item: TOutline[0]["dest"]) => void }> = memo(
+  ({ outline, onClick }) => {
+    const renderOutline = (outline: IOutline[], options: { firstLevel?: boolean; isSkeleton?: boolean } = {}) => {
+      const { firstLevel = false, isSkeleton = false } = options;
+
+      return (
+        <ul className={twMerge(firstLevel ? "mt-0" : "my-3")}>
+          {outline.map((item, index) => (
+            <li key={item.title} className={twMerge(firstLevel ? "pl-0 mt-4" : "pl-4", "mt-0.5 first-of-type:mt-0")}>
+              {isSkeleton && <OutlineLinkSkeleton className={twMerge("h-4.5", index % 3 == 0 && "w-52", index % 3 == 1 && "w-48", index % 3 == 2 && "w-32", index % 3 == 3 && "w-12", !firstLevel && "mt-0.5")} />}
+              {!isSkeleton && <Link
+                dest={item.dest}
+                onClick={onClick}
+                className={twMerge(
+                  "underline underline-offset-2",
+                  !firstLevel && "dark:text-[var(--brand-light)] text-[var(--brand-darkest)] mt-0.5",
+                  firstLevel && "dark:text-[var(--brand)] text-[var(--brand-darkest-2)]",
+                )}
+              >
+                {firstLevel && item.title.replace(".", " > ")}
+                {!firstLevel && item.title}
+              </Link>}
+              {item.items.length > 0 ? renderOutline(item.items, { isSkeleton }) : null}
+            </li>
+          ))}
+        </ul>
+      );
+    };
+
+    const pickedOutline = outline ?? outlineForSkeleton;
 
     return (
-      <ul className={twMerge(firstLevel ? "mt-0" : "my-3")}>
-        {outline.map((item) => (
-          <li key={item.title} className={twMerge(firstLevel ? "pl-0 mt-4" : "pl-4", "mt-0.5 first-of-type:mt-0")}>
-            <Link
-              dest={item.dest}
-              onClick={onClick}
-              className={twMerge(
-                "underline underline-offset-2",
-                !firstLevel && "dark:text-[var(--brand-light)] text-[var(--brand-darkest)] mt-0.5",
-                firstLevel && "dark:text-[var(--brand)] text-[var(--brand-darkest-2)]",
-              )}
-            >
-              {firstLevel && item.title.replace(".", " > ")}
-              {!firstLevel && item.title}
-            </Link>
-            {item.items.length > 0 ? renderOutline(item.items) : null}
-          </li>
-        ))}
-      </ul>
+      <div className="rounded-lg min-h-0 w-full py-6 px-6  bg-[#eeeeee] dark:bg-[#323232]  overflow-y-auto">
+        {renderOutline(pickedOutline, { firstLevel: true, isSkeleton: pickedOutline === outlineForSkeleton })}
+      </div>
     );
-  };
-
-  return (
-    <div className="rounded-lg min-h-0 w-full py-6 px-6  bg-[#eeeeee] dark:bg-[#323232]  overflow-y-auto">
-      {renderOutline(outline, { firstLevel: true })}
-    </div>
-  );
-})
+  },
+);
 
 OutlineDumb.displayName = "OutlineDumb";
 
@@ -100,8 +119,14 @@ type ILinkProps = {
 
 function Link({ dest, children, className, onClick }: ILinkProps) {
   return (
-    <a onClick={()=> dest && onClick(dest)} className={twMerge("cursor-pointer hover:opacity-75", className)}>
+    <a onClick={() => dest && onClick(dest)} className={twMerge("cursor-pointer hover:opacity-75", className)}>
       {children}
     </a>
   );
 }
+
+const OutlineLinkSkeleton: FC<{className: string}>= ({className}) => {
+  return (
+    <div className={twMerge("h-4 w-24 bg-gray-300/85 dark:bg-[var(--brand)] dark:opacity-25 rounded-md animate-pulse", className)}></div>
+  );
+};
