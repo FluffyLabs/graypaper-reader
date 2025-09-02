c50ed6373654ac3a78e6cc0ca0c2002335d41888
Wojciech Piątkowski <chmurson@gmail.com>
2025-09-02 18:33:52 +0200

    fix: looks of header on different sizes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,24 @@
-import { Header as FluffyHeader } from "@fluffylabs/shared-ui";
+import { Header as FluffyHeader, useBreakpoint } from "@fluffylabs/shared-ui";
 import type React from "react";
 import { Version } from "../Version";
 import toolLogoUrl from "./../../assets/tool-logo.svg";
 import { NotesButtonsGroup } from "./components";
 
 export const Header: React.FC = () => {
+  const isLargeOrWider = useBreakpoint("(width > 64rem)");
+  const isCustomMediumOrWider = useBreakpoint("(width > 52rem)");
+
   return (
     <FluffyHeader
       toolNameSrc={toolLogoUrl}
       ghRepoName="graypaper-reader"
       endSlot={
         <>
-          <div className="flex pl-4">
-            <NotesButtonsGroup className="max-sm:hidden" />
+          <div className="rounded-md flex ml-4 z-10 dark bg-[var(--card)]">
+            {isCustomMediumOrWider && <NotesButtonsGroup className="max-sm:hidden" />}
             <Version />
           </div>
-          <FluffyHeader.GithubDropdownMenu className="max-sm:hidden" />
+          {isLargeOrWider && <FluffyHeader.GithubDropdownMenu className="max-sm:hidden" />}
         </>
       }
     />
