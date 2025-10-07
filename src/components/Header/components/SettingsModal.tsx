095148943ab6e6550461f5ce81267faece52071a
Wojciech Piątkowski <chmurson@gmail.com>
2025-10-07 16:28:53 +0200

    style: replace custom local dialog with one from shared-ui

--- src/components/Header/components/SettingsModal.tsx
+++ src/components/Header/components/SettingsModal.tsx
@@ -1,5 +1,5 @@
+import { Button, Dialog, DialogModal } from "@fluffylabs/shared-ui";
 import { useContext } from "react";
-import Modal from "react-modal";
 import { type INotesContext, NotesContext } from "../../NotesProvider/NotesProvider";
 import { RemoteSources } from "../../RemoteSources/RemoteSources";
 
@@ -7,30 +7,18 @@ export const SettingsModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: (
   const { remoteSources, handleSetRemoteSources } = useContext(NotesContext) as INotesContext;
 
   return (
-    <Modal style={modalStyles} isOpen={isOpen} onRequestClose={onClose} contentLabel="Settings">
-      <button className="default-button float-right" onClick={onClose}>
-        ✖︎
-      </button>
-      <div className="text-2xl font-bold mb-4">Settings</div>
-
-      <RemoteSources remoteSources={remoteSources} onChange={handleSetRemoteSources} />
-
-      <br />
-      <button className="default-button settings-close" onClick={onClose}>
-        close
-      </button>
-    </Modal>
+    <DialogModal open={isOpen}>
+      <DialogModal.Content className="max-w-2xl">
+        <Dialog.Header>Settings</Dialog.Header>
+        <Dialog.Content>
+          <RemoteSources remoteSources={remoteSources} onChange={handleSetRemoteSources} />
+        </Dialog.Content>
+        <Dialog.Footer className="flex gap-2">
+          <Button variant="outline" onClick={onClose}>
+            Close
+          </Button>
+        </Dialog.Footer>
+      </DialogModal.Content>
+    </DialogModal>
   );
-};
-const modalStyles = {
-  content: {
-    minWidth: "50vw",
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-  overlay: { zIndex: 5 },
 };
