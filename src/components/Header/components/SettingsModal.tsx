f3f14894201d725ef9c5f9dcc329f274af8d2d1d
Wojciech Piątkowski <chmurson@gmail.com>
2025-10-08 17:43:13 +0200

    chore: udpate shared-ui and use new modal dialog api

--- src/components/Header/components/SettingsModal.tsx
+++ src/components/Header/components/SettingsModal.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogModal } from "@fluffylabs/shared-ui";
+import { Button, DialogModal } from "@fluffylabs/shared-ui";
 import { useContext } from "react";
 import { type INotesContext, NotesContext } from "../../NotesProvider/NotesProvider";
 import { RemoteSources } from "../../RemoteSources/RemoteSources";
@@ -9,15 +9,15 @@ export const SettingsModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: (
   return (
     <DialogModal open={isOpen}>
       <DialogModal.Content className="max-w-2xl">
-        <Dialog.Header>Settings</Dialog.Header>
-        <Dialog.Content>
+        <DialogModal.Title>Settings</DialogModal.Title>
+        <DialogModal.Body>
           <RemoteSources remoteSources={remoteSources} onChange={handleSetRemoteSources} />
-        </Dialog.Content>
-        <Dialog.Footer className="flex gap-2">
+        </DialogModal.Body>
+        <DialogModal.Footer className="flex gap-2">
           <Button variant="outline" onClick={onClose}>
             Close
           </Button>
-        </Dialog.Footer>
+        </DialogModal.Footer>
       </DialogModal.Content>
     </DialogModal>
   );
